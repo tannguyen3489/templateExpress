871ec9875e3e47b90b3d266c72890d880225fd86
kevin <tannguyen3489@gmail.com>
2016-08-27 20:49:54 +0700

    init express mvc

---
+++ config/env/development.js
@@ -0,0 +1,10 @@
+module.exports = {
+// Development configuration options
+//     db: 'mongodb://localhost:27017/mean-book',
+    sessionSecret: 'developmentSessionSecret',
+    // google: {
+    //     clientID: '459479244564-92k56529hvsjck72nbvipvict8trgmj7.apps.googleusercontent.com',
+    //     clientSecret: 'ViQmxT8lIgG0HJ5Uo-edFJCf',
+    //     callbackURL: 'http://localhost:3000/oauth/google/callback'
+    // }
+};
---
+++ config/express.js
@@ -0,0 +1,39 @@
+var config = require('./config'),
+    express = require('express'),
+    morgan = require('morgan'),
+    compress = require('compression'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    session = require('express-session');
+// flash = require('connect-flash'),
+// passport = require('passport');
+
+module.exports = function () {
+    var app = express();
+    if (process.env.NODE_ENV === 'development') {
+        app.use(morgan('dev'));
+    } else if (process.env.NODE_ENV === 'production') {
+        app.use(compress());
+    }
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    app.use(bodyParser.json());
+    app.use(methodOverride());
+
+    app.set('views', './app/views');
+    app.set('view engine', 'ejs');
+
+    app.use(session({
+        saveUninitialized: true,
+        resave: true,
+        secret: config.sessionSecret
+    }));
+
+
+    app.use(express.static('./public'));
+    app.use("/bower_components", express.static('./bower_components'));
+
+    require('../app/routes/index.js')(app);
+    return app;
+};
