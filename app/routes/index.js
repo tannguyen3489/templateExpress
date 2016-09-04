764e2af16e4271edafe73b55e6845eecc3f78628
kevin <tannguyen3489@gmail.com>
2016-09-04 21:21:47 +0700

    add bower

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,10 +2,10 @@ var index = require('../controllers/index.server.controller');
 
 module.exports = function (app) {
   app.get('/api', index.api);
-  app.get('/', index.react);
+  app.get('/', index.home);
   // app.get('/*', index.render);
   app.get('/formal', index.formal);
-  app.get('/home', index.home);
+  app.get('/', index.home);
   app.get('/about', index.about);
   app.get('/showcase', index.showcase);
   app.get('/product', index.product);
